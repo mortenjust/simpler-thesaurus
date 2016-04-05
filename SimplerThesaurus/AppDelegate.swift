8f8a058b386eaec1fe8da7ad017f455f5fef3d94
mortenjust <morten.just@genstart.dk>
2016-04-04 21:35:41 -0700

    Initial Commit

---
+++ SimplerThesaurus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimplerThesaurus
+//
+//  Created by Morten Just Petersen on 4/4/16.
+//  Copyright © 2016 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimplerThesaurus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimplerThesaurus
+//
+//  Created by Morten Just Petersen on 4/4/16.
+//  Copyright © 2016 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
